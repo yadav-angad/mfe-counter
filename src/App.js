7b7825500a3476289b19b11d0bd33e96fe58faa3
yadav-angad <yadavangad100@gmail.com>
2025-08-03 23:38:05 -0500

    Added text

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ export default function () {
   return (
     <Grid xs={12} sm={4} md={4} lg={4} xl={4} item>
       <Card sx={{ backgroundColor: '#f5f5f5', padding: '10px' }}>
-        <Alert severity="info">{'MFE 2 getting data from Host'}</Alert>
+        <Alert severity="info">{'MFE 1 Counter App, sharing data to Host Application'}</Alert>
         <Box>
           <Button sx={{ backgroundColor: 'DarkGreen', color: 'white', '&:hover': { backgroundColor: 'Green' } }} onClick={incrementCounter}>
             {'Increment'}
